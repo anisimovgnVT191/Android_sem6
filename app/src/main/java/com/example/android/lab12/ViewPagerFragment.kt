f2bee97b07852c40cd2f210bed3ccda20f04fd81
tewat <hollywoodstreets23@gmail.com>
2022-02-18 19:34:58 +0300

    Icons added to tabs

--- app/src/main/java/com/example/android/lab12/ViewPagerFragment.kt
+++ app/src/main/java/com/example/android/lab12/ViewPagerFragment.kt
@@ -1,5 +1,6 @@
 package com.example.android.lab12
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -32,9 +33,26 @@ class ViewPagerFragment : Fragment() {
         val tabLayout = binding.tabsLayout
         TabLayoutMediator(tabLayout, viewPager){tab, position ->
             tab.text = "Item $position"
+            tab.icon = iconFromPosition(position)
         }.attach()
     }
 
+    private fun iconFromPosition(position: Int) =
+        with(resources){
+            when(position){
+                0 -> getDrawable(R.drawable.ic_baseline_sports_volleyball_24)
+                1 -> getDrawable(R.drawable.ic_baseline_sports_tennis_24)
+                2 -> getDrawable(R.drawable.ic_baseline_sports_soccer_24)
+                3 -> getDrawable(R.drawable.ic_baseline_sports_rugby_24)
+                4 -> getDrawable(R.drawable.ic_baseline_sports_motorsports_24)
+                5 -> getDrawable(R.drawable.ic_baseline_sports_mma_24)
+                6 -> getDrawable(R.drawable.ic_baseline_sports_kabaddi_24)
+                7 -> getDrawable(R.drawable.ic_baseline_sports_hockey_24)
+                8 -> getDrawable(R.drawable.ic_baseline_sports_handball_24)
+                9 -> getDrawable(R.drawable.ic_baseline_sports_golf_24)
+                else -> getDrawable(R.drawable.ic_baseline_disabled_by_default_24)
+            }
+        }
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
