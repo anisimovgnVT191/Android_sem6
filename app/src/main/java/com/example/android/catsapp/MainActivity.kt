b191f8ba65fef71bab051bbe3425bfa928bed61f
tewat <hollywoodstreets23@gmail.com>
2022-03-17 16:35:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/catsapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.android.catsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.e("onCreate", BuildConfig.CATSAPI_KEY)
+    }
+}
