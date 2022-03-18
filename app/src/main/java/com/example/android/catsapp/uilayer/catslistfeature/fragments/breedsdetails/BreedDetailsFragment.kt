08422d3f5bd48a1b1828913c3132379f9e7870ab
tewat <hollywoodstreets23@gmail.com>
2022-03-18 21:02:08 +0300

    Half of visual polish

--- app/src/main/java/com/example/android/catsapp/uilayer/catslistfeature/fragments/breedsdetails/BreedDetailsFragment.kt
+++ app/src/main/java/com/example/android/catsapp/uilayer/catslistfeature/fragments/breedsdetails/BreedDetailsFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.android.catsapp.R
 import com.example.android.catsapp.databinding.FragmentBreedDetailsBinding
@@ -53,6 +54,7 @@ class BreedDetailsFragment : Fragment(R.layout.fragment_breed_details) {
         binding.detailsRecycler.apply {
             layoutManager = LinearLayoutManager(this.context)
             adapter = compositeAdapter
+            addItemDecoration(DividerItemDecoration(this@BreedDetailsFragment.context, LinearLayoutManager.VERTICAL))
         }
 
         return binding.root
